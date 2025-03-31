8d79bc3450231fd76fa48b51ab786c5d89323adc
Wu Clan <jianhengwu0407@gmail.com>
2025-03-31 23:38:20 +0800

    feat: create vben project (#6)
    * feat: create vben project
    
    * feat: adapt the login interface

---
+++ README.md
@@ -0,0 +1,39 @@
+# FastAPI Best Architecture UI
+
+Front-end Implementation of
+the [FastAPI Best Architecture](https://github.com/fastapi-practices/fastapi_best_architecture)
+
+## Run
+
+```shell
+pnpm install
+pnpm dev
+```
+
+## Build
+
+```shell
+pnpm build
+```
+
+## Contributors
+
+<a href="https://github.com/fastapi-practices/fba_ui/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=fastapi-practices/fba_ui"/>
+</a>
+
+## Special thanks
+
+- [Vue.js](https://cn.vuejs.org/guide/introduction.html)
+- [Vben Admin](https://www.vben.pro/)
+- ...
+
+## Sponsor us
+
+If this program has helped you, you can sponsor us with some coffee
+beans: [:coffee: Sponsor :coffee:](https://wu-clan.github.io/sponsor/)
+
+## License
+
+This project is licensed under the terms of
+the [MIT](https://github.com/fastapi-practices/fba_ui/blob/master/LICENSE) license
---
+++ apps/web-antd/src/views/_core/authentication/login.vue
@@ -0,0 +1,124 @@
+<script lang="ts" setup>
+import type { VbenFormSchema } from '@vben/common-ui';
+import type { BasicOption } from '@vben/types';
+
+import { computed, h, ref } from 'vue';
+
+import { AuthenticationLogin, z } from '@vben/common-ui';
+import { $t } from '@vben/locales';
+
+import { Image } from 'ant-design-vue';
+
+import { useAuthStore } from '#/store';
+
+defineOptions({ name: 'Login' });
+
+const authStore = useAuthStore();
+
+const MOCK_USER_OPTIONS: BasicOption[] = [
+  {
+    label: 'Admin',
+    value: 'admin',
+  },
+  {
+    label: 'Test',
+    value: 'test',
+  },
+];
+
+const imageSrc = ref('');
+const refreshCaptcha = async () => {
+  try {
+    const captcha = await authStore.captcha();
+    imageSrc.value = `data:image/png;base64, ${captcha}`;
+  } catch {
+    // 不做处理
+  }
+};
+refreshCaptcha();
+
+const formSchema = computed((): VbenFormSchema[] => {
+  return [
+    {
+      component: 'VbenSelect',
+      componentProps: {
+        options: MOCK_USER_OPTIONS,
+        placeholder: $t('authentication.selectAccount'),
+      },
+      fieldName: 'selectAccount',
+      label: $t('authentication.selectAccount'),
+      rules: z
+        .string()
+        .min(1, { message: $t('authentication.selectAccount') })
+        .optional()
+        .default('admin'),
+    },
+    {
+      component: 'VbenInput',
+      componentProps: {
+        placeholder: $t('authentication.usernameTip'),
+      },
+      dependencies: {
+        trigger(values, form) {
+          if (values.selectAccount) {
+            const findUser = MOCK_USER_OPTIONS.find(
+              (item) => item.value === values.selectAccount,
+            );
+            if (findUser) {
+              form.setValues({
+                password: '123456',
+                username: findUser.value,
+              });
+            }
+          }
+        },
+        triggerFields: ['selectAccount'],
+      },
+      fieldName: 'username',
+      label: $t('authentication.username'),
+      rules: z.string().min(1, { message: $t('authentication.usernameTip') }),
+    },
+    {
+      component: 'VbenInputPassword',
+      componentProps: {
+        placeholder: $t('authentication.password'),
+      },
+      fieldName: 'password',
+      label: $t('authentication.password'),
+      rules: z.string().min(1, { message: $t('authentication.passwordTip') }),
+    },
+    {
+      component: 'VbenInput',
+      componentProps: {
+        placeholder: $t('page.auth.captchaPlaceholder'),
+      },
+      fieldName: 'captcha',
+      label: $t('authentication.password'),
+      rules: z.string().length(4, { message: $t('page.auth.captchaRequired') }),
+      formItemClass: 'w-2/3',
+    },
+    {
+      component: h(Image),
+      componentProps: {
+        src: imageSrc.value,
+        width: 120,
+        height: 40,
+        preview: false,
+        onClick: refreshCaptcha,
+      },
+      fieldName: 'captchaImg',
+      formItemClass: 'ml-auto -mt-16',
+    },
+  ];
+});
+</script>
+
+<template>
+  <AuthenticationLogin
+    :form-schema="formSchema"
+    :loading="authStore.loginLoading"
+    :show-code-login="false"
+    :show-qrcode-login="false"
+    @submit="authStore.authLogin"
+  />
+</template>
