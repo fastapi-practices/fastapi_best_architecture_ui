e8fc15392466fac287af6d2c5d198265c5c10fb4
Wu Clan <jianhengwu0407@gmail.com>
2025-05-07 02:17:15 +0800

    feat: update api core folder some files

--- apps/web-antd/src/api/core/auth.ts
+++ apps/web-antd/src/api/core/auth.ts
@@ -1,51 +1,57 @@
-import { baseRequestClient, requestClient } from '#/api/request';
+import {
+  authRequestClient,
+  baseRequestClient,
+  requestClient,
+} from '#/api/request';
 
-export namespace AuthApi {
-  /** 登录接口参数 */
-  export interface LoginParams {
-    password?: string;
-    username?: string;
-  }
+export interface CaptchaResult {
+  image: string;
+  image_type: string;
+}
 
-  /** 登录接口返回值 */
-  export interface LoginResult {
-    accessToken: string;
-  }
+export interface LoginParams {
+  username: string;
+  password: string;
+  captcha: string;
+}
 
-  export interface RefreshTokenResult {
-    data: string;
-    status: number;
-  }
+export interface LoginResult {
+  access_token: string;
+}
+
+export type RefreshTokenResult = LoginResult;
+
+/**
+ * 登录验证码
+ */
+export async function getCaptchaApi() {
+  return requestClient.get<CaptchaResult>('/api/v1/auth/captcha');
 }
 
 /**
  * 登录
  */
-export async function loginApi(data: AuthApi.LoginParams) {
-  return requestClient.post<AuthApi.LoginResult>('/auth/login', data);
+export async function loginApi(data: LoginParams) {
+  return requestClient.post<LoginResult>('/api/v1/auth/login', data);
 }
 
 /**
  * 刷新accessToken
  */
 export async function refreshTokenApi() {
-  return baseRequestClient.post<AuthApi.RefreshTokenResult>('/auth/refresh', {
-    withCredentials: true,
-  });
+  return baseRequestClient.post<RefreshTokenResult>('/api/v1/token/new');
 }
 
 /**
  * 退出登录
  */
 export async function logoutApi() {
-  return baseRequestClient.post('/auth/logout', {
-    withCredentials: true,
-  });
+  return authRequestClient.post('/api/v1/auth/logout');
 }
 
 /**
  * 获取用户权限码
  */
 export async function getAccessCodesApi() {
-  return requestClient.get<string[]>('/auth/codes');
+  return [];
 }
--- apps/web-antd/src/api/core/user.ts
+++ apps/web-antd/src/api/core/user.ts
@@ -2,9 +2,14 @@ import type { UserInfo } from '@vben/types';
 
 import { requestClient } from '#/api/request';
 
+export interface MyUserInfo extends UserInfo {
+  id: number;
+  nickname: string;
+}
+
 /**
  * 获取用户信息
  */
 export async function getUserInfoApi() {
-  return requestClient.get<UserInfo>('/user/info');
+  return requestClient.get<MyUserInfo>('/api/v1/sys/users/me');
 }
