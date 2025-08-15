9c37c7e5374d2580403be36cd6e95e6cc4044352
Wu Clan <jianhengwu0407@gmail.com>
2025-08-15 15:30:45 +0800

    chore: updated config and profile translations

--- apps/web-antd/src/router/routes/modules/profile.ts
+++ apps/web-antd/src/router/routes/modules/profile.ts
@@ -1,13 +1,15 @@
 import type { RouteRecordRaw } from 'vue-router';
 
+import { $t } from '@vben/locales';
+
 const routes: RouteRecordRaw[] = [
   {
     name: 'Profile',
     path: '/profile',
     component: () => import('#/views/_core/profile/index.vue'),
     meta: {
+      title: $t('page.menu.profile'),
       icon: 'mingcute:profile-line',
-      title: '个人中心',
       hideInMenu: true,
     },
   },
