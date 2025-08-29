aaed3db2af00401e5dd1dc811fd2b3fc22e6aa91
Wu Clan <jianhengwu0407@gmail.com>
2025-08-29 13:28:37 +0800

    feat: add GitHub and Google OAuth2

--- apps/web-antd/src/plugins/oauth2/api/index.ts
+++ apps/web-antd/src/plugins/oauth2/api/index.ts
@@ -1,15 +1,13 @@
 import { requestClient } from '#/api/request';
 
-export interface OAuth2CallBackParams {
-  code: string;
-  state?: string;
-  code_verifier?: string;
+export async function getOAuth2Github() {
+  return requestClient.get<string>('/api/v1/oauth2/github');
+}
+
+export async function getOAuth2Google() {
+  return requestClient.get<string>('/api/v1/oauth2/google');
 }
 
 export async function getOAuth2LinuxDo() {
   return requestClient.get<string>('/api/v1/oauth2/linux-do');
-}
-
-export async function getOAuth2LinuxDoCallback(params: OAuth2CallBackParams) {
-  return requestClient.get('/api/v1/oauth2/linux-do/callback', { params });
 }
--- apps/web-antd/src/plugins/oauth2/views/login.vue
+++ apps/web-antd/src/plugins/oauth2/views/login.vue
@@ -1,13 +1,36 @@
 <script setup lang="ts">
+import { MdiGithub, MdiGoogle } from '@vben/icons';
 import { $t } from '@vben/locales';
 
-import { getOAuth2LinuxDo } from '#/plugins/oauth2/api';
+import {
+  getOAuth2Github,
+  getOAuth2Google,
+  getOAuth2LinuxDo,
+} from '#/plugins/oauth2/api';
 
 defineOptions({ name: 'OAuth2Login' });
 
-const linuxDoOAuth2 = async () => {
+interface OAuth2Source {
+  source: 'Github' | 'Google' | 'LinuxDo';
+}
+
+const OAuth2 = async (oa: OAuth2Source) => {
   try {
-    window.location.href = await getOAuth2LinuxDo();
+    switch (oa.source) {
+      case 'Github': {
+        window.location.href = await getOAuth2Github();
+        break;
+      }
+      case 'Google': {
+        window.location.href = await getOAuth2Google();
+        break;
+      }
+      case 'LinuxDo': {
+        window.location.href = await getOAuth2LinuxDo();
+        break;
+      }
+      // No default
+    }
   } catch (error) {
     console.error(error);
   }
@@ -23,10 +46,19 @@ const linuxDoOAuth2 = async () => {
       </span>
       <span class="border-input w-[35%] border-b dark:border-gray-600"></span>
     </div>
-
-    <div class="mt-4 flex flex-wrap justify-center gap-2">
-      <a-button class="mb-3" shape="circle" type="ghost" @click="linuxDoOAuth2">
-        <img src="https://linux.do/logo-25.svg" />
+    <div class="mt-5 flex flex-wrap justify-center">
+      <a-button class="mb-3" type="ghost" @click="OAuth2({ source: 'Github' })">
+        <MdiGithub class="size-6" />
+      </a-button>
+      <a-button class="mb-3" type="ghost" @click="OAuth2({ source: 'Google' })">
+        <MdiGoogle class="size-6" />
+      </a-button>
+      <a-button
+        class="mb-3"
+        type="ghost"
+        @click="OAuth2({ source: 'LinuxDo' })"
+      >
+        <img src="https://linux.do/logo-24.svg" />
       </a-button>
     </div>
   </div>
