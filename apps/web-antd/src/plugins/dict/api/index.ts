85e6d885ae974c838b0cce9213a190ea00676319
Wu Clan <jianhengwu0407@gmail.com>
2025-09-03 19:04:56 +0800

    feat: add the dict view

--- apps/web-antd/src/plugins/dict/api/index.ts
+++ apps/web-antd/src/plugins/dict/api/index.ts
@@ -1,3 +1,109 @@
-export interface Todo {
-  key: string;
+import type { PaginationResult } from '#/types';
+
+import { requestClient } from '#/api/request';
+
+export interface DictTypeParams {
+  name?: string;
+  code?: string;
+  status?: number;
+  page?: number;
+  size?: number;
+}
+
+export interface DictTypeResult {
+  id: number;
+  name: string;
+  code: string;
+  status: number;
+  remark?: string;
+  created_time: string;
+  updated_time?: string;
+}
+
+export interface CreateDictTypeParams {
+  name: string;
+  code: string;
+  status: number;
+  remark: string;
+}
+
+export interface DictDataParams {
+  type_id?: number;
+  label?: string;
+  status?: number;
+}
+
+export interface DictDataResult {
+  id: number;
+  type_id: number;
+  label: string;
+  value: string;
+  sort: number;
+  status: number;
+  remark: string;
+}
+
+export interface CreateDictDataParams {
+  type_id: number;
+  label: string;
+  value: string;
+  sort: number;
+  status: number;
+  remark?: string;
+}
+
+export async function getAllDictTypeApi() {
+  return await requestClient.get<DictTypeResult[]>(
+    '/api/v1/sys/dict-types/all',
+  );
+}
+
+export async function getDictTypeListApi(params: DictTypeParams) {
+  return await requestClient.get<PaginationResult<DictTypeResult>>(
+    '/api/v1/sys/dict-types',
+    { params },
+  );
+}
+
+export async function createDictTypeApi(data: CreateDictTypeParams) {
+  return await requestClient.post('/api/v1/sys/dict-types', data);
+}
+
+export async function updateDictTypeApi(
+  pk: number,
+  data: CreateDictTypeParams,
+) {
+  return await requestClient.put(`/api/v1/sys/dict-types/${pk}`, data);
+}
+
+export async function deleteDictTypeApi(pks: number[]) {
+  return await requestClient.post('/api/v1/sys/dict-types', { data: { pks } });
+}
+
+export async function getAllDictDataApi() {
+  return await requestClient.get<DictDataResult[]>(
+    '/api/v1/sys/dict-datas/all',
+  );
+}
+
+export async function getDictDataListApi(params: DictDataParams) {
+  return await requestClient.get<PaginationResult<DictDataResult>>(
+    '/api/v1/sys/dict-datas',
+    { params },
+  );
+}
+
+export async function createDictDataApi(data: CreateDictDataParams) {
+  return await requestClient.post('/api/v1/sys/dict-datas', data);
+}
+
+export async function updateDictDataApi(
+  pk: number,
+  data: CreateDictDataParams,
+) {
+  return await requestClient.put(`/api/v1/sys/dict-datas/${pk}`, data);
+}
+
+export async function deleteDictDataApi(pks: number[]) {
+  return await requestClient.post('/api/v1/sys/dict-datas', { data: { pks } });
 }
--- apps/web-antd/src/plugins/dict/views/index.vue
+++ apps/web-antd/src/plugins/dict/views/index.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
-import ComingSoon from '#/views/_core/fallback/coming-soon.vue';
+import { ColPage } from '@vben/common-ui';
+
+import DictData from '#/plugins/dict/views/dict-data.vue';
+import DictType from '#/plugins/dict/views/dict-type.vue';
 </script>
 
 <template>
-  <ComingSoon />
+  <ColPage
+    auto-content-height
+    :resizable="false"
+    :left-width="40"
+    :right-width="60"
+  >
+    <template #left>
+      <div class="mr-2 h-full">
+        <DictType />
+      </div>
+    </template>
+    <DictData />
+  </ColPage>
 </template>
